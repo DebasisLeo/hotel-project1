63054eac1c8d6ed672f879b8b7ea20703dc25722
debasis <joydebashis8@gmail.com>
2024-12-23 22:06:34 +0600

    eleventh commit

--- firebase.init.js
+++ firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-l9xzxpiQq46StUVW2wJWTMim9PLzGPM",
-  authDomain: "hotel-managemet.firebaseapp.com",
-  projectId: "hotel-managemet",
-  storageBucket: "hotel-managemet.firebasestorage.app",
-  messagingSenderId: "417474334668",
-  appId: "1:417474334668:web:af7c22d1a517df678f5769"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
